fde893bb95a6c4b9254ba8ad6c2516f928e00f86
Alex Maiorella <35647226+donkeyteethUX@users.noreply.github.com>
2018-02-08 12:02:21 -0600

    added functionality to extract time spent/added a few comments

--- extract_answers.py
+++ extract_answers.py
@@ -13,7 +13,7 @@ import sys
 import re
 
 def main(evals_file, question_file):
-
+    csv.field_size_limit(sys.maxsize)
     evals = []
     with open(evals_file) as f:
         reader = csv.reader(f)
@@ -29,15 +29,34 @@ def main(evals_file, question_file):
     instructor = re.compile("\nInstructor\(s\): (.*)\n")
 
     for e in evals:
+        response_dict = {}
         e_list = e.split('\n')
+
         i = instructor.match(e_list[4])
-        for line in e_list:
-            if line in question_list:
-                question = line
-                response_dict[question] = []
+        response_dict['instructor'] = i
+
+        for i, line in enumerate(e_list):
+
+            # extracts the time demands info, should work well
+            if 'How many hours per week did you' in line:
+                time = []
+                time.append(e.split('\n')[i+1:i+4])
+                response_dict['time_stats'] = time
+
+            try:
+                q = re.match('.*\?', line).group(0)
+                if q in question_list:
+                    response_dict[q] = []
+                    # insert some clever shit here
+
+
+            except:
+                pass
+
             if line not in question_list:
                 if len(response_dict) > 0:
                     response_dict[question].append(line)
+            # what should we do with 'response_dict'? Write to a json file maybe?
 
 
 if __name__ == '__main__':
