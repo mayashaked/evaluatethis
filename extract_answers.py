60cfbb062b32eb77c04f66d9a10d6524fb689391
Alex Maiorella <35647226+donkeyteethUX@users.noreply.github.com>
2018-02-14 14:17:29 -0600

    extract answers is decently functional now

--- extract_answers.py
+++ extract_answers.py
@@ -11,7 +11,7 @@
 import csv
 import sys
 import re
-import json 
+import json
 
 def main(evals_file, all_question_file, course_q, instructor_q):
     #import all evals
@@ -28,7 +28,7 @@ def main(evals_file, all_question_file, course_q, instructor_q):
     with open(all_question_file, encoding = 'ISO-8859-1') as f:
         reader = csv.reader(f)
         for row in reader:
-            question_list.append(row)
+            question_list.append(row[0])
 
     course_qs = []
     with open(course_q, encoding = 'ISO-8859-1') as f:
@@ -44,30 +44,40 @@ def main(evals_file, all_question_file, course_q, instructor_q):
 
 
     #compile regular expression to search for instructor name
-    instructor = re.compile("\nInstructor\(s\): (.*)\n")
+    instructor = re.compile("Instructor\(s\): (.*)")
+    eval_list = []
 
-    for e in evals:
+    for e in evals[10:30]:
         num_responses = int(e.split('\n')[7][-2:])
         in_question = False
         answers = []
         response_dict = {}
         e_list = e.split('\n') #splits evaluations into lines
 
-        prof = instructor.match(e_list[4]) #the name of the instructor is always on the fourth line of the evaluation
-        response_dict['instructor'] = prof
+        prof = instructor.match(e_list[5]) #the name of the instructor is always on the fourth line of the evaluation
+        response_dict['instructor(s)'] = None
+        if prof:
+            response_dict['instructor(s)'] = prof.group(1).split('; ')
+        course_info_match = re.match('([A-Z]{4}) ([0-9]{5}): (.*)', e_list[3])
+        response_dict['responses'] = num_responses
+        response_dict['dept'] = course_info_match.group(1)
+        response_dict['course_number'] = course_info_match.group(2)
+        response_dict['course'] = course_info_match.group(3)
 
+        responses_found = 0
         for i, line in enumerate(e_list):
-
-            if stopping_cond(line):
-                response_dict[q].append(answers)
+            if in_question and stopping_cond(line, question_list, responses_found, num_responses):
+                response_dict[q].extend(answers)
                 in_question = False
 
             if in_question:
                 answers.append(line)
+                responses_found += 1
 
             for l in course_qs:
                 if l in line:
-                    q = 'course'
+                    q = 'course_responses'
+                    response_dict[q] = []
                     in_question = True
                     answers = []
                     responses_found = 0
@@ -75,7 +85,9 @@ def main(evals_file, all_question_file, course_q, instructor_q):
 
             for l in instructor_qs:
                 if l in line:
-                    q = 'instructor'
+                    print(l)
+                    q = 'instructor_responses'
+                    response_dict[q] = []
                     in_question = True
                     answers = []
                     responses_found = 0
@@ -84,19 +96,25 @@ def main(evals_file, all_question_file, course_q, instructor_q):
             # extracts the time info, works independently
             if 'How many hours per week did you' in line:
                 time = []
-                time.append(e.split('\n')[i+1:i+4])
+                time.extend(e.split('\n')[i+1:i+4])
                 response_dict['time_stats'] = time
 
-
         eval_list.append(response_dict)
-        return eval_list
-    
-def stopping_cond():
+        print(response_dict)
+    return eval_list
+
+def stopping_cond(line, question_list, responses_found, num_responses):
     if responses_found == num_responses:
         return True
 
-    if 
+    if re.search('[0-9][0-9]? / [0-9][0-9]?%', line):
+        return True
 
+    for q in question_list:
+        if q in line:
+            return True
+
+    return False
 
     #writes to json
     '''
@@ -106,4 +124,7 @@ def stopping_cond():
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    try:
+        main('C:/Users/alex/Desktop/unique_evals.csv', 'C:/Users/alex/Desktop/manually_cleaned_eval_questions.csv', 'C:/Users/alex/Desktop/course_quality_questions.csv', 'C:/Users/alex/Desktop/instructor_quality_questions.csv')
+    except:
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
