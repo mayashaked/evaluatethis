823767d3acd842110001570fdb4015ee66faead7
Alex Maiorella <alexmaiorella@cs.uchicago.edu>
2018-02-15 18:21:18 -0600

    Fully functioning first version

--- extract_answers.py
+++ extract_answers.py
@@ -13,21 +13,21 @@ import sys
 import re
 import json
 
-EXACT_MATCH_ONLY = ['overall', 'explain', 'the content material', 'useful?',
+EXACT_MATCH_ONLY = {'overall', 'explain', 'the content material', 'useful?',
  'exams', 'the tests', 'the textbook', 'this course', 'the homework assignments',
- 'very useful', 'labs', 'level', 'the instructor', 'Useful?', 'Texts?', 'the assignments', 'Weaknesses?', 'Strengths?']
+ 'very useful', 'labs', 'level', 'the instructor','texts?', 'additional comments'
+ 'the assignments', 'weaknesses?', 'strengths?', 'anyone interested in the topic', 'written comments', 'which least?'}
 
-def main(evals_file, all_question_file, course_q, instructor_q):
-    #import all evals
+def main(evals_file, all_question_file, course_q, instructor_q, file):
+
     csv.field_size_limit(sys.maxsize)
+
     evals = []
     with open(evals_file, encoding = 'ISO-8859-1') as f:
         reader = csv.reader(f)
         for row in reader:
             evals.append(row[0])
 
-    #import list of questions from a text file
-    #list of questions is made with some wild trickery in another file
     question_list = []
     with open(all_question_file, encoding = 'ISO-8859-1') as f:
         reader = csv.reader(f)
@@ -46,30 +46,44 @@ def main(evals_file, all_question_file, course_q, instructor_q):
         for row in reader:
             instructor_qs.append(row[0])
 
+    eval_list = iterate(evals, question_list, course_qs, instructor_qs)
+    write_to_json(eval_list, file)
+
+
+def iterate(evals, question_list, course_qs, instructor_qs):
 
     #compile regular expression to search for instructor name
-    instructor = re.compile("Instructor\(s\): (.*)")
+    instructor_re = re.compile("Instructor\(s\): ?(.*)")
+    course_info_re = re.compile('([A-Z]{4}) ([0-9]{5}): ?(.*)')
+    num_responses_re = re.compile('esponses: ?([0-9]*)')
     eval_list = []
 
-    for e in evals[:500]:
-        num_responses = int(e.split('\n')[7][-2:])
+    for e in evals:
+        e_list = e.split('\n') #splits evaluations into lines
         in_question = False
         answers = []
         response_dict = {}
-        e_list = e.split('\n') #splits evaluations into lines
+        
+        for header_line in e_list[:10]: # deal seperately with header rows to avoid conflicts
 
-        prof = instructor.match(e_list[5]) #the name of the instructor is always on the fourth line of the evaluation
-        response_dict['instructor(s)'] = None
-        if prof:
-            response_dict['instructor(s)'] = prof.group(1).split('; ')
-        course_info_match = re.match('([A-Z]{4}) ([0-9]{5}): (.*)', e_list[3])
-        response_dict['responses'] = num_responses
-        response_dict['dept'] = course_info_match.group(1)
-        response_dict['course_number'] = course_info_match.group(2)
-        response_dict['course'] = course_info_match.group(3)
+            if course_info_re.match(header_line):
+                course_info_match = course_info_re.match(header_line)
+                response_dict['dept'] = course_info_match.group(1)
+                response_dict['course_number'] = course_info_match.group(2)
+                response_dict['course'] = course_info_match.group(3)
+
+            if instructor_re.match(header_line):
+                prof = instructor_re.match(header_line)
+                response_dict['instructor(s)'] = prof.group(1).split('; ')
+
+            if num_responses_re.search(header_line):
+                num_responses = num_responses_re.search(header_line)
+                response_dict['num_responses'] = num_responses.group(1)
+
 
         responses_found = 0
         for i, line in enumerate(e_list):
+
             if in_question and stopping_cond(line, question_list, responses_found, num_responses):
                 response_dict[q].extend(answers)
                 in_question = False
@@ -88,7 +102,7 @@ def main(evals_file, all_question_file, course_q, instructor_q):
                     break
 
             for l in instructor_qs:
-                if l in line or line in ['Weaknesses?', 'Strengths?']:
+                if l in line or line.lower() in ['weaknesses?', 'strengths?']:
                     q = 'instructor_responses'
                     response_dict[q] = []
                     in_question = True
@@ -108,32 +122,38 @@ def main(evals_file, all_question_file, course_q, instructor_q):
         eval_list.append(response_dict)
     return eval_list
 
+    
+
 def stopping_cond(line, question_list, responses_found, num_responses):
-    if responses_found == num_responses:
+    if '©' in line:
+        return True
+
+    if num_responses != 0 and responses_found == num_responses: # Don't trust response number if it says zero
         return True
 
     if re.search('[0-9][0-9]? / [0-9][0-9]?%', line):
         return True
 
-    if line in EXACT_MATCH_ONLY:
+    if line.lower() in EXACT_MATCH_ONLY:
         return True
 
     for q in question_list:
-        if q in line:
-            if q != line:
-                print((q,line))
+        if q.lower() in line.lower():
+            '''if q != line and 'Strengths' not in line and 'Weaknesses' not in line:
+                                                    print((q,line))'''
+            return True
 
     return False
 
-    #writes to json
-    '''
-    with open('eval_list', 'w') as outfile:
-    json.dump(eval_list, outfile)
-    '''
+
+def write_to_json(eval_list, file):
+    
+    with open(file, 'w', encoding = 'ISO-8859-1') as outfile:
+        json.dump(eval_list, outfile)
+    
 
 
 if __name__ == '__main__':
-    try:
-        main('C:/Users/alex/Desktop/unique_evals.csv', 'C:/Users/alex/Desktop/manually_cleaned_eval_questions.csv', 'C:/Users/alex/Desktop/course_quality_questions.csv', 'C:/Users/alex/Desktop/instructor_quality_questions.csv')
-    except:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    main('/home/alexmaiorella/Downloads/unique_evals.csv', '/home/alexmaiorella/Downloads/manually_cleaned_eval_questions.csv', 
+        '/home/alexmaiorella/Downloads/course_quality_questions.csv', '/home/alexmaiorella/Downloads/instructor_quality_questions.csv',
+        '/home/alexmaiorella/Downloads/evals_json_version_1')
