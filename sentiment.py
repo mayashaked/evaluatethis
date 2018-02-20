b2c2e4f8fa4f1def41c4003e64ba42883fb926b4
Lily Li <23038037+mouyun-lily-li@users.noreply.github.com>
2018-02-20 04:59:40 -0600

    Updated sentiment analysis code

--- sentiment.py
+++ sentiment.py
@@ -1,25 +1,38 @@
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+### FIRST INSTALL vaderSentiment package
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import pandas as pd
 
-sid = SentimentIntensityAnalyzer()
+# to do:
+# 1. determine how sql queries connect to this
+# 2. generate scores for prof, department, and course (over all sections and 
+# quarters) for comparison
 
-with open('sampledict.json') as json_data:
-	alldicts = json.load(json_data)
+data = pd.read_json('evals_json_version_4_part1.dms')
+analyzer = SentimentIntensityAnalyzer()
 
-for evaluation in alldicts
-		polar = 0
-		neg = 0
-		neu = 0
-		pos = 0
-	for question in evaluation:
-		sentences = evaluation[question]
-		for sentence in sentences:
-			ss = sid.polarity_scores(sentence)
-			polar += ss['compound']
-			neg += ss['neg']
-			pos += ss['pos']
-			neu += ss['neu']
-	print('polar: ' + polar/len(evaluation))
-	print('negative: ' + neg/len(evaluation))
-	print('neutral: ' + neu/len(evaluation))
-	print('positive: ' + pos/len(evaluation))
-		
+course = data.loc[2] # EXAMPLE ONLY, actual course will come from sql query
+
+course_responses_scores = {'compound': 0, 'neg': 0, 'neu': 0, 'pos': 0}
+instructor_responses_scores = {'compound': 0, 'neg': 0, 'neu': 0, 'pos': 0}
+
+# calculate total sentiment scores
+for eval_ in course.course_responses:
+    scores = analyzer.polarity_scores(eval_)
+    course_responses_scores['compound'] += scores['compound'] # not sure if this measure is useful/necessary
+    course_responses_scores['neg'] += scores['neg']
+    course_responses_scores['neu'] += scores['neu']
+    course_responses_scores['pos'] += scores['pos']
+
+for eval_ in course.instructor_responses:
+    scores = analyzer.polarity_scores(eval_)
+    instructor_responses_scores['compound'] += scores['compound']
+    instructor_responses_scores['neg'] += scores['neg']
+    instructor_responses_scores['neu'] += scores['neu']
+    instructor_responses_scores['pos'] += scores['pos']
+
+# calculate average sentiment scores
+for key, value in course_responses_scores.items():
+    course_responses_scores[key] /= len(course.course_responses)
+
+for key, value in instructor_responses_scores.items():
+    instructor_responses_scores[key] /= len(course.instructor_responses)
