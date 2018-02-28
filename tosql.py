0e008815a54c71414f8268d69851adf1fe87e64b
mayashaked <mayashaked@uchicago.edu>
2018-02-28 17:01:50 -0600

    added rec/dont rec cols for inst

--- tosql.py
+++ tosql.py
@@ -1,138 +1,137 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      alex
-#
-# Created:     18/02/2018
-# Copyright:   (c) alex 2018
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
-# The goal here is to reduce agree/disagree type data of the following form into
-# a single float representing a score for the intructor/course overall/
-# assignments.
-
-###################################################################
-
-# 'N/A|Strongly Disagree|Disagree|Neutral|Agree|Stongly Agree'
-# 'Presented clear lectures. 5% 0% 0% 0% 21% 73%'
-# 'Held my attention and made this course interesting. 0% 0% 0% 5% 47% 47%'
-# 'Stimulated and facilitated questions and discussions. 0% 0% 0% 5% 21% 73%'
-# 'Responded well to student questions. 0% 0% 0% 0% 10% 89%'
-# 'Was available outside of class. 33% 0% 0% 5% 16% 44%'
-# 'Was helpful during office hours. 50% 0% 0% 0% 11% 38%'
-# 'Motivated independent thinking. 0% 0% 0% 5% 31% 63%',
-# 'Made me want to take another course from him or her. 0% 0% 0% 15% 15% 68%'
-
-###################################################################
-import re
 import pandas as pd
-import numpy as np
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import sqlite3
+import aggregate_numerical_data as agg_num
 
-def add_score_cols(df):
-
-    sia = SentimentIntensityAnalyzer()
-
-    # initialize new columns, 'concat' them later
-    # we find this approach runs faster than adding values to df 1 at a time
-    emp = list([np.nan for a in range(len(df))])
-    inst_sentiment_col = pd.Series(emp, name = 'inst_sentiment')
-    assignments_score_col = pd.Series(emp, name = 'assignments_score')
-    readings_score_col = pd.Series(emp, name = 'readings_score_col')
-    instructor_score_col = pd.Series(emp, name = 'instructor_score')
-    tests_score_col = pd.Series(emp, name = 'tests_score')
-    course_sentiment_col = pd.Series(emp, name = 'course_sentiment')
-    overall_score_col = pd.Series(emp, name = 'overall_score')
-
-    for row in df.iterrows():
-
-        # The following complication is neccesary because some physics
-        # evaluations have agree...disagree rather than vice versa
-        reverse_order = False
-        if row[1].dept == 'PHYS':
-            reverse_order = True
-        if row[1].dept == 'PHSC':
-            reverse_order = 'maybe'
-
-        if type(row[1].The_Instructor) == list:
-            instructor_score = compute_numerical_score(row[1].The_Instructor, reverse_order)
-            instructor_score_col[row[0]] = instructor_score
-        if type(row[1].The_Assignments) == list:
-            assignments_score = compute_numerical_score(row[1].The_Assignments, reverse_order)
-            assignments_score_col[row[0]] = assignments_score
-        if type(row[1].The_Tests) == list:
-            tests_score = compute_numerical_score(row[1].The_Tests, reverse_order)
-            tests_score_col[row[0]] = tests_score
-        if type(row[1].Overall) == list:
-            overall_score = compute_numerical_score(row[1].Overall, reverse_order)
-            overall_score_col[row[0]] = overall_score
-        if type(row[1].The_Readings) == list:
-            readings_score = compute_numerical_score(row[1].The_Readings, reverse_order)
-            readings_score_col[row[0]] = readings_score
-
-        if type(row[1].course_responses) == list:
-            length = len(row[1].course_responses)
-            if length >= 1:
-                course_sentiment = round((np.mean([sia.polarity_scores(c)['compound'] for c in row[1].course_responses]) + 1)*50, 2)
-                course_sentiment_col[row[0]] = round(weight_sent_scores(course_sentiment, length, 'course'), 1)
-
-        if type(row[1].instructor_responses) == list:
-            length = len(row[1].instructor_responses)
-            if length >= 1:
-                inst_sentiment = round((np.mean([sia.polarity_scores(c)['compound'] for c in row[1].instructor_responses]) + 1)*50, 2)
-                inst_sentiment_col[row[0]] = round(weight_sent_scores(inst_sentiment, length, 'inst'), 1)
-
-    return pd.concat([df, inst_sentiment_col, assignments_score_col, overall_score_col, instructor_score_col, tests_score_col, course_sentiment_col, readings_score_col], axis = 1)
+EVALS_PART_1 = 'evals_json_version_5_part1'
+EVALS_PART_2 = 'evals_json_version_5_part2'
+SQL_DB_PATH = 'reevaluations.db'
 
 
-def compute_numerical_score(data, reverse_order):
+db = sqlite3.connect(SQL_DB_PATH)
+j1 = pd.read_json(EVALS_PART_1, convert_dates = False)
+j2 = pd.read_json(EVALS_PART_2, convert_dates = False)
 
-    scores = []
-    denominator = []
+j = pd.concat([j1, j2])
+j = j.set_index('unique_id')
 
-    for line in data:
-        p = [int(a) for a in re.findall('([0-9][0-9]?[0-9]?)%', line)]
-        if reverse_order == True:
-            p.reverse()
+#aggregate numerical scores re: tests, instructor, readings, assignments, as 
+#well as sentiment analysis scores
 
-        if len(p) == 6:
-        # first element is N/A percentage; don't factor it into weighted score/possible score
-            weighted_score = p[1]+p[2]*2+p[3]*3+p[4]*4+p[5]*5
-            possible_score = sum(p[1:])*5
+j = agg_num.add_score_cols(j)
 
-        elif len(p) == 5:
-            weighted_score = p[0]+p[1]*2+p[2]*3+p[3]*4+p[4]*5
-            possible_score = sum(p)*5
+j['year'] = j['year'].fillna(-1).astype(int)
+j['section'] = j['section'].fillna(-1).astype(int)
+j['course_number'] = j['course_number'].fillna(-1).astype(int)
+j['num_responses'] = j['num_responses'].fillna(-1).astype(int)
+j['low_time'] = j['low_time'].fillna(-1).astype(float)
+j['avg_time'] = j['avg_time'].fillna(-1).astype(float)
+j['high_time'] = j['high_time'].fillna(-1).astype(float)
 
-        else:
-            return np.nan
 
-        scores.append(weighted_score)
-        denominator.append(possible_score)
+j = j.where(j != -1, None)
 
-    if sum(denominator) == 0 or not data:
-        return np.nan
 
-    score = round(sum(scores) / sum(denominator) * 100, 1)
 
-    # Make an educated guess that the order should be reversed, then recalculate
-    if score <= 50 and reverse_order == 'maybe':
-        return compute_numerical_score(data, True)
+def gen_courses(j, db):
 
-    return score
+    courses = j[['course', 'course_number', 'dept', 'section', 'term', 'year']]
 
-def weight_sent_scores(raw_score, num_responses, category):
+    sqldbcourses = courses.to_sql('courses', con = db, flavor = 'sqlite', index = True, index_label = 'course_id')
 
-    average = {'inst' : 75, 'course' : 62.5}
+    pass
 
-    if num_responses in range(1,5):
-        normalized_raw_score = raw_score - average[category]
-        adjusted = normalized_raw_score * (1 - .15 * (5 - num_responses))
-        return adjusted + average[category]
+def gen_profs(j, db):
 
-    return raw_score
 
-if __name__ == '__main__':
-    main(df)
+    profs = []
+    for ind, row in j.iterrows():
+        if type(row['instructors']) == list:
+            for prof in row['instructors']:
+                fullname = prof.split(', ')
+                profs.append([ind, fullname[0], fullname[-1]])
+
+    profs = pd.DataFrame(profs)
+    profs = profs.rename(columns = {0 : 'course_id', 1: "ln", 2 : "fn"})
+    sqldbprofs = profs.to_sql('profs', con = db, flavor = 'sqlite', index = False)
+
+    pass
+
+def gen_crosslists(j, db):
+
+    crosslists = []
+    for ind, row in j.iterrows():
+        if type(row['identical_courses']) == str:
+            x = row['identical_courses'].split(', ')
+            for course in x:
+                crosslists.append([ind, course])
+                
+    crosslists = pd.DataFrame(crosslists)
+    crosslists = crosslists.rename(columns = {0 : 'course_id', 1 : 'crosslist'})
+    sqldbcrosslists = crosslists.to_sql('crosslists', con = db, flavor = 'sqlite', index = False)
+
+    pass
+
+def gen_evals(j, db):
+
+    evals = []
+    for ind, row in j.iterrows():
+        eval = [ind, None, None, None, None, 0, None, None, None, None, None, None, None, None, None, None]
+        if row['instructor_score'] != None:
+            if row['instructor_score'] > -1:
+                eval[1] = row['instructor_score']
+        if row['assignments_score'] != None:
+            if row['assignments_score'] > -1:
+                eval[2] = row['assignments_score']
+        if row['overall_score'] != None:
+            if row['overall_score'] > -1:
+                eval[3] = row['overall_score']
+        if row['tests_score'] != None:
+            if row['tests_score'] > -1:
+                eval[4] = row['tests_score']
+        eval[5] = row['num_responses']
+        if row['low_time'] != None:
+            if row['low_time'] > -1:
+                eval[6] = row['low_time']
+        if row['avg_time'] != None:
+            if row['avg_time'] > -1:
+                eval[7] = row['avg_time']
+        if row['high_time'] != None:
+            if row['high_time'] > -1:
+                eval[8] = row['high_time']
+        if type(row['recommend']) == list:
+            eval[9] = int(row['recommend'][0])
+            eval[10] = int(row['recommend'][1])
+        if row['inst_sentiment'] != None:
+            if row['inst_sentiment'] > -1:
+                eval[11] = row['inst_sentiment']
+        if row['course_sentiment'] != None:
+            if row['course_sentiment'] > -1:
+                eval[12] = row['course_sentiment']
+        if row['readings_score_col'] != None:
+            if row['readings_score_col'] > -1:
+                eval[13] = row['readings_score_col']
+        if type(row['good_instructor']) == list:
+            eval[14] = int(row['good_instructor'][0])
+            eval[15] = int(row['good_instructor'][1])
+
+
+        evals.append(eval)
+
+    evals = pd.DataFrame(evals)
+
+    evals = evals.rename(columns = {0 : 'course_id', 1 : 'prof_score', 2 : 'ass_score', \
+        3 : 'over_score' , 4 : 'test_score', 5 : 'num_responses', \
+        6 : 'low_time', 7 : 'avg_time', 8 : 'high_time', 9 : 'num_recommend', \
+        10 : 'num_dont_recommend', 11 : 'inst_sentiment', 12 : 'course_sentiment',
+        13 : 'read_score', 14 : 'good_inst', 15 : 'bad_inst'})
+
+    sqldbevals = evals.to_sql('evals', con = db, flavor = 'sqlite', index = False)
+
+    pass
+
+
+gen_courses(j, db)
+gen_profs(j, db)
+gen_crosslists(j, db)
+gen_evals(j, db)
+
