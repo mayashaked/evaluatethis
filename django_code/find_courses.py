bcb0d58f21fbc4df672d7241b9a957e32981c9f9
Sam Hoffman <samanthahoffman@cs.uchicago.edu>
2018-03-01 15:12:53 -0600

    this is a commit message

--- django_code/find_courses.py
+++ django_code/find_courses.py
@@ -5,3 +5,22 @@
 # course and professor- dropdown or box to type?
 # do we want to return multiple results with links like the current eval website or gather the info for just one course?
 # can we do course comparisons?
+
+import sqlite3
+import os
+
+DATA_DIR = os.path.dirname(__file__)
+DATABASE_FILENAME = os.path.join(DATA_DIR, 'evaluations/reevaluations.db')
+
+
+db = sqlite3.connect(DATABASE_FILENAME)
+
+def find_by_dept():
+	query_string = "SELECT courses.dept, courses.course, evals.over_score, evals.prof_score, courses.year \
+	FROM courses JOIN profs JOIN evals ON profs.course_id = courses.course_id AND \
+	profs.course_id = evals.course_id WHERE courses.year < 2004;"
+	return query_string
+
+def find_by_course_name:
+	
+
