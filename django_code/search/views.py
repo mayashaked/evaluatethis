ed80ba18f318d79689e5de9f67baa11953bdc36c
mayashaked <mayashaked@uchicago.edu>
2018-03-03 14:41:42 -0600

    Update views.py

--- django_code/search/views.py
+++ django_code/search/views.py
@@ -3,7 +3,9 @@ import traceback
 import sys
 import csv
 import os
+import io
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from functools import reduce
 from operator import and_
@@ -11,7 +13,7 @@ from operator import and_
 from django.shortcuts import render
 from django import forms
 
-from courses import find_courses
+from courses import find_courses, get_wc
 
 NOPREF_STR = 'No preference'
 RES_DIR = os.path.join(os.path.dirname(__file__), '..', 'res')
@@ -20,6 +22,23 @@ COLUMN_NAMES = dict(
     course_num = 'Course Number',
     prof = 'Professor',
 )
+
+
+# def _valid_result(res):
+#     """Validate results returned by find_courses."""
+#     (HEADER, RESULTS) = [0, 1]
+#     ok = (isinstance(res, (tuple, list)) and
+#           len(res) == 2 and
+#           isinstance(res[HEADER], (tuple, list)) and
+#           isinstance(res[RESULTS], (tuple, list)))
+#     if not ok:
+#         return False
+
+#     n = len(res[HEADER])
+
+#     def _valid_row(row):
+#         return isinstance(row, (tuple, list)) and len(row) == n
+#     return reduce(and_, (_valid_row(x) for x in res[RESULTS]), True)
 
 
 def _load_column(filename, col=0):
@@ -39,94 +58,86 @@ def _build_dropdown(options):
     return [(x, x) if x is not None else ('', NOPREF_STR) for x in options]
 
 
-# create the dropdown menus that appear on the website 
 DEPTS = _build_dropdown([None] + _load_res_column('dept_list.csv'))
 COURSE_NUMS = _build_dropdown([None] + _load_res_column('course_num_list.csv'))
 PROFS_FN = _build_dropdown([None] + _load_res_column('prof_fn_list.csv'))
 PROFS_LN = _build_dropdown([None] + _load_res_column('prof_ln_list.csv'))
 
 
-class SearchForm_course_dept(forms.Form):
+class SearchForm_course(forms.Form):
     dept = forms.ChoiceField(label='Department', choices=DEPTS, required=False)
-
-
-class SearchForm_course_num(forms.Form):
     course_num = forms.ChoiceField(label='Course Number', choices=COURSE_NUMS, required=False)
 
 
-class SearchForm_prof_fn(forms.Form):
+class SearchForm_prof(forms.Form):
     prof_fn = forms.ChoiceField(label='Professor\'s First Name', choices=PROFS_FN, required=False)
-
-
-class SearchForm_prof_ln(forms.Form):
     prof_ln = forms.ChoiceField(label='Professor\'s Last Name', choices=PROFS_LN, required=False)
+    show_args = forms.BooleanField(label='Show args_to_ui',
+                                   required=False) # delete this in end
 
 
-# the main function. it gathers the data from the dropdown menus we defined 
-# above, cleans the data, checks if the input queries are valid, and returns
-# outputs (tables, graphs, images) to be rendered in the html text
 def home(request):
     context = {}
     res = None
     if request.method == 'GET':
         # create a form instance and populate it with data from the request:
-        form_course_dept = SearchForm_course_dept(request.GET)
-        form_course_num = SearchForm_course_num(request.GET)
-        form_prof_fn = SearchForm_prof_fn(request.GET)
-        form_prof_ln = SearchForm_prof_ln(request.GET)
-        # check whether the forms are valid and convert form data to an args 
-        # dictionary for find_courses
+        form_course = SearchForm_course(request.GET)
+        form_prof = SearchForm_prof(request.GET)
+        # check whether it's valid:
         args = {}
-        if form_course_dept.is_valid():
-            dept = form_course_dept.cleaned_data['dept']
+        if form_course.is_valid():
+            # Convert form data to an args dictionary for find_courses
+            dept = form_course.cleaned_data['dept']
             if dept:
                 args['dept'] = dept
-        if form_course_num.is_valid():
-            course_num = form_course_num.cleaned_data['course_num']
+            course_num = form_course.cleaned_data['course_num']
             if course_num:
                 args['course_num'] = course_num
-        if form_prof_fn.is_valid():   
-            prof_fn = form_prof_fn.cleaned_data['prof_fn']
+            
+        if form_prof.is_valid():   
+            prof_fn = form_prof.cleaned_data['prof_fn']
             if prof_fn:
                 args['prof_fn'] = prof_fn
-        if form_prof_ln.is_valid():   
-            prof_ln = form_prof_ln.cleaned_data['prof_ln']
+            prof_ln = form_prof.cleaned_data['prof_ln']
             if prof_ln:
                 args['prof_ln'] = prof_ln
+            if form_prof.cleaned_data['show_args']:
+                context['args'] = 'args_to_ui = ' + json.dumps(args, indent=2)
 
-        # check the conditional inputs of the dropdown menus
-        if ('dept' in args) == ('course_num' in args): 
-        #either they were both inputs or neither were inputs
-            if ('prof_fn' in args) == ('prof_ln' in args):
-                try:
-                    res = find_courses(args) # inputs are valid, run courses.py
-                except Exception as e:
-                    print('Exception caught')
-                    bt = traceback.format_exception(*sys.exc_info()[:3])
-                    context['err'] = """
-                    An exception was thrown in find_courses:
-                    <pre>{}
-                    {}</pre>
-                    """.format(e, '\n'.join(bt))
-            else:
-                context['err'] = forms.ValidationError("To search by professor, \
-                    both first name and last name are required.")
+            try:
+                res = find_courses(args) #result of courses.py
+                context["wordcloud"] = None
+                # result of courses.py
+                '''wc = get_wc(args)
+                context["wordcloud"] = wc
+                f = io.BytesIO()
+                plt.figure(figsize = (15, 8))
+                plt.imshow(wc)
+                plt.axis("off")
+                plt.savefig(f, format = "png")
+                plt.clf()'''
+
+
+            except Exception as e:
+                print('Exception caught')
+                bt = traceback.format_exception(*sys.exc_info()[:3])
+                context['err'] = """
+                An exception was thrown in find_courses:
+                <pre>{}
+{}</pre>
+                """.format(e, '\n'.join(bt))
+
                 res = None
-        else:
-            context['err'] = forms.ValidationError("To search by course, both \
-                department and course number are required.")
-            res = None
-
     else:
         form = SearchForm()
 
     # Handle different responses of res
     if res is None:
         context['result'] = None
-    # elif isinstance(res, str):
-    #     context['result'] = None
-    #     context['err'] = res
-    #     result = None
+    elif isinstance(res, str):
+        context['result'] = None
+        context['err'] = res
+        result = None
     # elif not _valid_result(res):
     #     context['result'] = None
     #     context['err'] = ('Return of find_courses has the wrong data type. '
@@ -142,8 +153,6 @@ def home(request):
         context['result'] = result
         context['num_results'] = len(res)
    
-    context['form_course_dept'] = form_course_dept
-    context['form_course_num'] = form_course_num
-    context['form_prof_fn'] = form_prof_fn
-    context['form_prof_ln'] = form_prof_ln
+    context['form_course'] = form_course
+    context['form_prof'] = form_prof
     return render(request, 'index.html', context)
