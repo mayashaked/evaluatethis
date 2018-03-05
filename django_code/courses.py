21c6bac7206ab680ab61808c03003024d6666840
Alex Maiorella <alexmaiorella@cs.uchicago.edu>
2018-03-05 16:21:32 -0600

    finalized courses.py and worked on graphs

--- django_code/courses.py
+++ django_code/courses.py
@@ -6,6 +6,7 @@ import pandas as pd
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
+from statistics import mode
 
 # Use this filename for the database
 DATA_DIR = os.path.dirname(__file__)
@@ -58,7 +59,7 @@ def get_wc(args_from_ui):
 
 
 
-def find_courses(args_from_ui):
+def find_courses(args):
 
     '''
     Takes a dictionary containing search criteria and returns courses
@@ -90,7 +91,10 @@ def find_courses(args_from_ui):
         elif 'prof_fn' in args and 'prof_ln' in args:
             prof = prof_query()
             dept = dept_query()
-            dept_df = pd.read_sql_query(dept.format(args['dept'], args['dept']), db)
+            print('here')
+            primary_dept = get_profs_primary_dept(args, db)
+            print(primary_dept)
+            dept_df = pd.read_sql_query(dept.format(primary_dept, primary_dept), db)
             prof_df = pd.read_sql_query(prof.format(args['prof_fn'], args['prof_ln']), db)
             return dept_df, prof_df
 
@@ -102,13 +106,13 @@ def find_courses(args_from_ui):
         dept_df = pd.read_sql_query(dept.format(args['dept'], args['dept']), db)
         prof_df = pd.read_sql_query(prof.format(args['prof_fn'], args['prof_ln']), db)
         course_df = pd.read_sql_query(course.format(args['dept'], args['dept'], args['course_num']), db)
-        course_and_prof_df = pd.read_sql_query(course_and_prof.format(args['dept'], args['dept'], args['course_num'], args['fn'], args['ln']), db)
+        course_and_prof_df = pd.read_sql_query(course_and_prof.format(args['dept'], args['dept'], args['course_num'], args['prof_fn'], args['prof_ln']), db)
         return dept_df, course_df, prof_df, course_and_prof_df
 
 
 def course_and_prof_query():
 
-    course_and_prof = "SELECT evals.*, course, fn, ln, \
+    course_and_prof = "SELECT evals.*, course, fn, ln \
                        FROM courses JOIN profs JOIN evals JOIN crosslists \
                        ON courses.course_id = evals.course_id \
                        AND courses.course_id = crosslists.course_id \
@@ -134,7 +138,7 @@ def course_query():
 
 def prof_query():
 
-    prof = "SELECT evals.*, course, fn, ln, \
+    prof = "SELECT evals.*, course, fn, ln \
             FROM courses JOIN profs JOIN evals JOIN crosslists \
             ON courses.course_id = evals.course_id \
             AND courses.course_id = crosslists.course_id \
@@ -178,3 +182,17 @@ def clean_header(s):
             break
 
     return s
+
+def get_profs_primary_dept(args, db):
+
+    cursor = db.cursor()
+    depts = cursor.execute('SELECT dept FROM courses JOIN profs ON \
+        profs.course_id = courses.course_id WHERE profs.fn = ? \
+        and profs.ln = ?', [args['prof_fn'], args['prof_ln']])
+    
+    try:
+        return mode([a[0] for a in depts])
+        return depts[0]
+    except:
+        return ''
+
--- django_code/graphs.py
+++ django_code/graphs.py
@@ -1,5 +1,6 @@
 import courses
 import pandas as pd
+import numpy as np
 '''
 get the necessary data given args from ui
 this assumes that you can search by prof, dept, or course number
@@ -8,9 +9,9 @@ if you search by dept, you don't want info about a specific course or prof
 '''
 
 def graph_it(args_from_ui):
-    if prof_fn in args_from_ui and prof_ln in args_from_ui and len(args_from_ui) == 2:
+    if "prof_fn" in args_from_ui and "prof_ln" in args_from_ui and len(args_from_ui) == 2:
         prof_graph(args_from_ui)
-    if dept in args_from_ui and course_num in args_from_ui and len(args_from_ui) == 2:
+    if "dept" in args_from_ui and "course_num" in args_from_ui and len(args_from_ui) == 2:
         course_graph(args_from_ui)
     else:
         course_prof_graph(args_from_ui)
@@ -24,8 +25,26 @@ def prof_graph(args_from_ui):
     ders = class in turkish, i ran out of names, sorry
     '''
     df = courses.find_courses(args_from_ui)
-    title = "Comparison of the time demands made by " + args_from_ui['prof_fn'] + ' ' args_from_ui['prof_ln'] + "to the departmental average"
-    course_df = df[df.course == ders].groupby(['course']).mean()
+    title = "Comparison of the time demands made by " + args_from_ui['prof_fn'] + ' ' + args_from_ui['prof_ln'] + " to the departmental average"
+    course_df = df.groupby(['course']).mean()
+    lows = course_df.low_time
+    avgs = course_df.avg_time
+    highs = course_df.high_time
+    n = lows.shape()[0]
+    ind = np.arange(n)
+    width = 0.35
+    plt.figure(figsize = (10, 5))
+    p1 = plt.bar(ind, lows, width, color='#d62728')
+    p2 = plt.bar(ind, avgs, width,
+             bottom=lows, color = '#f442cb')
+    p3 = plt.bar(ind, avgs, width,
+             bottom=avgs, color = '#63cbe8')
+    plt.ylabel('Amount of time spent')
+    plt.title(title)
+    plt.legend((p1[0], p2[0], p3[0]), ('Low', 'Average', 'High'))
+    plt.show()
+
+
 
 
 
@@ -52,33 +71,3 @@ def course_prof_graph(args_from_ui):
 
 
 
-
-def time_comparison(lows, avgs, highs, graph_type, course_name, dept, prof):
-    '''
-    graph_type options: time comparison (time), prof score comparison (prof)
-    lows, avgs, highs are tuples of those values for each group    
-    you can search for course and professor, just professor, just course
-    '''
-    if graph_type == "course_and_prof" or if graph_type == "course":
-        
-    if graph_type == "prof":
-        title = "Comparison of this professor's time demands to other related professors"
-    n = len(lows)
-    ind = np.arange(n)
-    width = 0.35
-    plt.figure(figsize = (10, 5))
-    p1 = plt.bar(ind, lows, width, color='#d62728')
-    p2 = plt.bar(ind, avgs, width,
-             bottom=lows, color = '#f442cb')
-    p3 = plt.bar(ind, avgs, width,
-             bottom=avgs, color = '#63cbe8')
-    plt.ylabel('Amount of time spent')
-    plt.title(title)
-    if graph_type == "course_and_prof":
-        plt.xticks(ind, (course_name + , course_name + ' over time', dept, prof))
-    if graph_type == "prof":
-        plt.xticks(ind, (prof, prof + ' over time', dept))
-
-    plt.legend((p1[0], p2[0], p3[0]), ('Low', 'Average', 'High'))
-    plt.show()
-    
