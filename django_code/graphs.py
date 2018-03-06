6c0cc1e4de1277b2fcfb4aba28dacf73b0c9b77a
Sam Hoffman <samanthahoffman@cs.uchicago.edu>
2018-03-06 14:41:38 -0600

    some progress was made

--- django_code/graphs.py
+++ django_code/graphs.py
@@ -1,6 +1,7 @@
 import courses
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 '''
 get the necessary data given args from ui
 this assumes that you can search by prof, dept, or course number
@@ -22,18 +23,17 @@ def prof_graph(args_from_ui):
     If the user searches by professor only, this code will produce a graph comparing
     the time demands of every course the professor has taught to the department average
     time demands.
-    ders = class in turkish, i ran out of names, sorry
     '''
-    df = courses.find_courses(args_from_ui)
+    deptdf, profdf = courses.find_courses(args_from_ui)
     title = "Comparison of the time demands made by " + args_from_ui['prof_fn'] + ' ' + args_from_ui['prof_ln'] + " to the departmental average"
-    course_df = df.groupby(['course']).mean()
+    course_df = profdf.groupby(['course']).mean()
     lows = course_df.low_time
     avgs = course_df.avg_time
     highs = course_df.high_time
-    n = lows.shape()[0]
+    n = lows.shape[0]
     ind = np.arange(n)
     width = 0.35
-    plt.figure(figsize = (10, 5))
+    plt.figure(figsize = (10, 10))
     p1 = plt.bar(ind, lows, width, color='#d62728')
     p2 = plt.bar(ind, avgs, width,
              bottom=lows, color = '#f442cb')
@@ -41,7 +41,11 @@ def prof_graph(args_from_ui):
              bottom=avgs, color = '#63cbe8')
     plt.ylabel('Amount of time spent')
     plt.title(title)
+    xnames = list(profdf.course.unique())
+    plt.xticks(ind, xnames, rotation = 90, fontsize = 6)
     plt.legend((p1[0], p2[0], p3[0]), ('Low', 'Average', 'High'))
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(bottom = .2)
     plt.show()
 
 
