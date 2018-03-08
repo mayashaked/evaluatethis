ad318b287734b2b45ac58a4812c12396b433ba32
Lily Li <23038037+mouyun-lily-li@users.noreply.github.com>
2018-03-08 04:35:18 -0600

    working version of website

--- django_code/search/urls.py
+++ django_code/search/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('', views.Course_Num_ListView.as_view(), name='Course_Num_changelist'),
-    # path('add/', views.Course_Num_CreateView.as_view(), name='Course_Num_add'),
-    # path('<int:pk>/', views.Course_Num_UpdateView.as_view(), name='Course_Num_change'),
 ]
